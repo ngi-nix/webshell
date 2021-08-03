48e585e8444363af6765d36d8677efeacb9fccb3
Mazurel <mateusz.mazur@yahoo.com>
2021-08-03 12:24:41 +0200

    WIP: Adding basic WebShell apps

--- flake.nix
+++ flake.nix
@@ -11,7 +11,12 @@
     flake = false;
   };
 
-  outputs = { self, nixpkgs, webshell-sandbox, napalm }:
+  inputs.webshell-app-textarea = {
+    url = "github:websh-org/app-textarea";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, webshell-sandbox, webshell-app-textarea, napalm }:
     let
       version = "0.2.1";
 
@@ -54,9 +59,49 @@
           buildInputs = [ nodePackages.parcel-bundler ];
 
           buildPhase = ''
-            cd _napalm-install/src
-            NODE_ENV=production parcel build index.html --out-dir=../docs --global WebShellSandbox
-            cd ..
+            cd _napalm-install
+
+            cd src; NODE_ENV=production parcel build index.html --out-dir=../docs; cd ..
+            # This is my custom script for testing server
+            # 
+            # It uses simple python server, which behaves similary
+            # to the github pages, which are used in production
+            mkdir bin
+            cat > bin/run.sh << EOL
+            #!/bin/sh
+            ${final.python3}/bin/python3 -m http.server --directory \\
+            EOL
+            echo $out >> bin/run.sh
+            chmod +x bin/run.sh
+          '';
+
+          installPhase = ''
+            mkdir -p $out/sandbox
+            cp -rd docs bin $out
+            cp -rd docs/* $out/sandbox
+          '';
+        };
+
+        app-textarea = let
+          textarea-data =
+            (napalm.overlay final prev).napalm.buildPackage webshell-app-textarea {
+              npmCommands = [ "npm install --ignore-scripts" "npm run build" ];
+              buildInputs = with final; [ nodePackages.parcel-bundler ];
+              packageLock = ./package-lock.json;
+            };
+        in with final;
+        stdenv.mkDerivation {
+          inherit version;
+          pname = "app-textarea";
+
+          src = textarea-data;
+
+          buildInputs = [ nodePackages.parcel-bundler ];
+
+          buildPhase = ''
+            cd _napalm-install
+
+            cd src; NODE_ENV=production parcel build index.html --public-url=/app-textarea --out-dir=../docs; cd ..
 
             # This is my custom script for testing server
             # 
@@ -65,21 +110,23 @@
             mkdir bin
             cat > bin/run.sh << EOL
             #!/bin/sh
-            ${final.python3}/bin/python3 -m http.server --directory ../docs
+            ${final.python3}/bin/python3 -m http.server --directory \\
             EOL
+            echo $out >> bin/run.sh
             chmod +x bin/run.sh
           '';
 
           installPhase = ''
-            mkdir -p $out
+            mkdir -p $out/app-textarea
             cp -rd docs bin $out
+            cp -rd docs/* $out/app-textarea
           '';
         };
       };
 
       # Provide some binary packages for selected system types.
       packages =
-        forAllSystems (system: { inherit (nixpkgsFor.${system}) sandbox; });
+        forAllSystems (system: { inherit (nixpkgsFor.${system}) sandbox app-textarea; });
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
@@ -90,6 +137,10 @@
       apps = forAllSystems (system:
         {
           sandbox = sandboxApp self system;
+          app-textarea = {
+            type = "app";
+            program = "${self.packages.${system}.app-textarea}/bin/run.sh";
+          };
         });
 
       defaultApp = forAllSystems (system: (sandboxApp self system));
